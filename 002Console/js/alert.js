39bcc3acde719e9a060929431a4b3748cb982b72
Gary <704992395@qq.com>
2019-01-12 23:31:02 +0800

    Modify

---
+++ 002Console/js/alert.js
@@ -0,0 +1,22 @@
+/*
+        alert("子曰：\n“二货不可多得”");
+*/
+/*
+        alert("子曰：\n“你也不可多得”");
+*/
+function woDeTaoLu(){
+
+    alert("taolu1===你猜一会儿会发生什么")
+    alert("taolu2===再等等")
+    alert("taolu3===见证奇迹的时刻到了")
+
+}
+alert("0=====你怎么打开这个网页的");
+alert("0=====我知道你有点好奇");
+
+woDeTaoLu();
+woDeTaoLu();
+woDeTaoLu();
+
+alert("0=====这是最后一句了");
+alert("0=====886");
